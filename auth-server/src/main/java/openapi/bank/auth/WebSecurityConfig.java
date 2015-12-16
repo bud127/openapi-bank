251281f64b711419a99801401bef85c64f9e5a81
chanwook <chanwook.god@gmail.com>
2015-12-16 13:41:37 +0900

    /oauth/authorize 통과. security ignore 설정에 "/oauth/**"로 되어 있어 auth 서버 로그인 페이지로 넘어가지 않는 문제였음!!

--- auth-server/src/main/java/openapi/bank/auth/WebSecurityConfig.java
+++ auth-server/src/main/java/openapi/bank/auth/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/images/**", "/oauth/**");
+        web.ignoring().antMatchers("/images/**");
     }
 
     @Bean
